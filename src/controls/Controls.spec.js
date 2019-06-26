3c87c0a17ca8491ea2d8e363be10e6d749a403ab
Marcelo Laos <mlaos1997@gmail.com>
2019-06-26 18:35:41 -0400

    refactored tests in controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -8,19 +8,19 @@ import Controls from './Controls';
 
 describe('<Controls />', () => {
     it('provides buttons to toggle the closed and locked states', () => {
-       const { getByText } = render(<Controls />);
+       const { getByText } = render(<Controls locked={false} closed={true}/>);
        
        const lockedButton = getByText(/lock gate/i);
-       const closedButton = getByText(/close gate/i);
+       const closedButton = getByText(/open gate/i);
     });
 
-    it('should change buttons text to reflect the state the door will be in if clicked', () => {
+    it('should change buttons text to reflect the state the door will be in if clicked', async () => {
         const toggleLocked = jest.fn();
         const toggleClosed = jest.fn();
 
-        const { getByText, queryByText } = render(<Controls locked={false} closed={true} toggleLocked={toggleLocked} toggleClosed={toggleClosed} />)
-        const lockedButton = getByText(/lock gate/i);
-        const closedButton = getByText(/open gate/i);
+        const { getByText, findByText } = render(<Controls locked={false} closed={true} toggleLocked={toggleLocked} toggleClosed={toggleClosed} />)
+        let lockedButton = getByText(/lock gate/i);
+        let closedButton = getByText(/open gate/i);
 
         fireEvent.click(lockedButton);
         fireEvent.click(closedButton);
@@ -28,10 +28,7 @@ describe('<Controls />', () => {
         expect(toggleLocked).toHaveBeenCalled();
         expect(toggleClosed).toHaveBeenCalled();
 
-        expect(queryByText(/unlock gate/i));
-        expect(queryByText(/close gate/i));
-
-
-
+        lockedButton = await expect(findByText(/unlock gate/i));
+        closedButton = await expect(findByText(/close gate/i));
     });
-})
+ });
