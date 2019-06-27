76a1f911c7f214b877449d6bfde15038c7dc703f
Marcelo Laos <mlaos1997@gmail.com>
2019-06-26 21:08:36 -0400

    completed mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,23 +1,27 @@
 // Test away!
 import React from 'react';
-import { render, fireEvent, getByText } from '@testing-library/react';
+import {render, fireEvent, getByText} from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each'
 
 import Controls from './Controls';
 
 describe('<Controls />', () => {
     it('provides buttons to toggle the closed and locked states', () => {
-       const { getByText } = render(<Controls locked={false} closed={true}/>);
-       
-       const lockedButton = getByText(/lock gate/i);
-       const closedButton = getByText(/open gate/i);
+        const {getByText} = render(<Controls locked={false} closed={true}/>);
+
+        const lockedButton = getByText(/lock gate/i);
+        const closedButton = getByText(/open gate/i);
     });
 
-    it('should change buttons text to reflect the state the door will be in if clicked', async () => {
+    it('should change buttons text to reflect the state the door will be in if clicked', async() => {
         const toggleLocked = jest.fn();
         const toggleClosed = jest.fn();
 
-        const { getByText, findByText } = render(<Controls locked={false} closed={true} toggleLocked={toggleLocked} toggleClosed={toggleClosed} />)
+        const {getByText, findByText} = render(<Controls
+            locked={false}
+            closed={true}
+            toggleLocked={toggleLocked}
+            toggleClosed={toggleClosed}/>)
         let lockedButton = getByText(/lock gate/i);
         let closedButton = getByText(/open gate/i);
 
@@ -30,4 +34,22 @@ describe('<Controls />', () => {
         lockedButton = await expect(findByText(/unlock gate/i));
         closedButton = await expect(findByText(/close gate/i));
     });
- });
+
+    it('should disable closed button if the gate is locked', () => {
+        const toggleLocked = jest.fn();
+        const toggleClosed = jest.fn();
+        const {getByTestId} = render(<Controls
+            toggleLocked={toggleLocked}
+            toggleClosed={toggleClosed}
+            closed={false}
+            locked={true}/>);
+        const lockedButton = getByTestId('lockedButton');
+        const closedButton = getByTestId('closedButton');
+
+        fireEvent.click(lockedButton);
+        fireEvent.click(closedButton);
+
+        expect(toggleLocked).toHaveBeenCalledTimes(0);
+        expect(toggleClosed).toHaveBeenCalledTimes(0);
+    })
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,37 +1,36 @@
 // Test away!
 import React from 'react';
-import { render } from 'react-testing-library';
+import {render} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Display from './Display';
 import '@testing-library/react/cleanup-after-each'
 
-
 describe('<Display />', () => {
     it('displays if gate is open/closed and if it is locked/unlocked', () => {
-        const { getByText } = render(<Display locked={true} closed={true} />);
+        const {getByText} = render(<Display locked={true} closed={true}/>);
         getByText('Locked');
         getByText('Closed');
     });
 
     it('displays closed if closed prop is true, and locked if lock props is true', () => {
-        const { getByText } = render(<Display locked={true} closed={true}/>);
+        const {getByText} = render(<Display locked={true} closed={true}/>);
         getByText('Closed');
         getByText('Locked');
     });
 
     it('displays open if closed prop is false, and unlocked if locked prop is false', () => {
-        const { getByText } = render(<Display locked={false} closed={false} />);
+        const {getByText} = render(<Display locked={false} closed={false}/>);
         getByText('Open');
         getByText('Unlocked');
     });
 
     it('uses the green-led class when unlocked or open', () => {
-        const { container } = render(<Display locked={false} closed={false} />);
+        const {container} = render(<Display locked={false} closed={false}/>);
         expect(container.children[0].children[0]).toHaveClass('led green-led');
     });
 
     it('uses the red-led class when locked or closed', () => {
-        const { container } = render(<Display locked={true} closed={true} />)
+        const {container} = render(<Display locked={true} closed={true}/>)
         expect(container.children[0].children[0]).toHaveClass('led red-led');
     })
 });
